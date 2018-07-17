1c37265610449d093da82fe64c448fe7261ec9df
Demo User <demouser@MacBook-Pro.local>
2018-07-17 15:42:52 -0500

    memeD

---
+++ memeD/javascript.js
@@ -0,0 +1,16 @@
+console.log('hello world');
+
+const name = 'charles';
+console.log(`hello ${name}!`);
+
+const age = 15;
+console.log(`you are ${age} years old`);
+
+if(age >= 18){
+  console.log(`vroom vroom and vote out cheetoe puff`);
+  }else {
+    if(age >= 15){
+      (`no vote but u can drive`);
+    }else{
+  console.log(`nah dog you taking the patita express`);
+}
